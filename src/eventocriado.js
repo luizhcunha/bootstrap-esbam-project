adf999446f560e15e07bd038c1e2dd77bb54d47b
luizhc <luiznascimento6@esbam.edu.br>
2024-04-15 00:02:09 -0400

    final commit

--- src/eventocriado.js
+++ src/eventocriado.js
@@ -1,34 +1,26 @@
+// Obter dados do localStorage
 const nome = localStorage.getItem('nome');
 const sobrenome = localStorage.getItem('sobrenome');
 const email = localStorage.getItem('email');
 const telefone = localStorage.getItem('telefone');
 const nomeEvento = localStorage.getItem('nomeEvento');
 const descricaoEvento = localStorage.getItem('descricaoEvento');
-
 const cep = localStorage.getItem('cep');
-const estado = localStorage.getItem('estado')
-const cidade = localStorage.getItem('cidade')
-const rua = localStorage.getItem('rua')
-const bairro = localStorage.getItem('bairro')
-const complemento = localStorage.getItem('complemento')
+const estado = localStorage.getItem('estado');
+const cidade = localStorage.getItem('cidade');
+const rua = localStorage.getItem('rua');
+const bairro = localStorage.getItem('bairro');
+const complemento = localStorage.getItem('complemento');
 
-const dataComoString = localStorage.getItem('dataEvento')
+// Construir nome completo
+const nomeCompleto = `${nome} ${sobrenome}`;
 
-const nomeCompleto = nome + " " + sobrenome;
-
-console.log(nomeCompleto);
-
-console.log(dataComoString);
-
-const textoNomeEvento = document.querySelector('#nomeEvento')
-const textoDescricaoEvento = document.querySelector('#descricaoEvento')
-const textoIdealizador = document.querySelector("#idealizadorEvento")
-
-// Textos de Contato
+// Selecionar elementos HTML
+const textoNomeEvento = document.querySelector('#nomeEvento');
+const textoDescricaoEvento = document.querySelector('#descricaoEvento');
+const textoIdealizador = document.querySelector("#idealizadorEvento");
 const textoContatoTelefone = document.querySelector("#contatoEventoZap");
 const textoContatoEmail = document.querySelector("#contatoEventoEmail");
-
-// Textos de Localização
 const textoCEP = document.querySelector("#cepEvento");
 const textoEstado = document.querySelector("#estadoEvento");
 const textoCidade = document.querySelector("#cidadeEvento");
@@ -36,20 +28,15 @@ const textoRua = document.querySelector("#ruaEvento");
 const textoBairro = document.querySelector("#bairroEvento");
 const textoComplemento = document.querySelector("#complementoEvento");
 
-
-// Inserindo no HTMl
+// Inserir dados nos elementos HTML
 textoNomeEvento.innerHTML = nomeEvento;
 textoDescricaoEvento.innerHTML = descricaoEvento;
-
 textoIdealizador.innerHTML = nomeCompleto;
-
 textoContatoTelefone.value = telefone;
 textoContatoEmail.value = email;
-
 textoCEP.value = cep;
 textoEstado.value = estado;
 textoCidade.value = cidade;
 textoRua.value = rua;
 textoBairro.value = bairro;
 textoComplemento.value = complemento;
-
